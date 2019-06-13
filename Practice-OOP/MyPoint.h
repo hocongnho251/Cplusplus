f9a0ed2b6710d745c50e3dba953b26dba716aa27
CongNho <hocongnhodtu@gmail.com>
2019-06-13 08:25:55 +0700

    - Modify varibl name for parameter constructor and write comment

--- Practice-OOP/MyPoint.h
+++ Practice-OOP/MyPoint.h
@@ -4,13 +4,14 @@ private:
 	int mPosX;
 	int mPosY;
 public:
-	MyPoint();
-	MyPoint(int _mPosX, int _mPosY);
+
+	MyPoint();//default constructor
+	MyPoint(int _mPosX, int _mPosY); //parameter constructor
 	void Display();
 	void SetX(int X);
 	int GetX();
 	void SetY(int Y);
 	int GetY();
-	double Distance(MyPoint p);
+	double Distance(MyPoint p);// Distance between 2 point
 
 };
